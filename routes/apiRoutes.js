ad2bfeb5ed4ea6f2ff5e5ef42e221bfc791c4801
Seiji Osada <seiji13r@outlook.com>
2018-12-04 01:01:42 -0600

    eslint updated adding es6 support Linting fixed in server.js and apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
-var request = require('request');
+var request = require("request");
 
-module.exports = function(app, ombd_key) {
+module.exports = function(app, ombdKey) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
@@ -18,17 +18,24 @@ module.exports = function(app, ombd_key) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({
+      where: { id: req.params.id }
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Search Movies
   app.post("/api/moviesearch", function(req, res) {
-    var movie = req.body
-    let entryUrl = "https://www.omdbapi.com/?s=" + movie.title + "&type=movie&apikey=" + ombd_key;
-    request(entryUrl, function (error, response, body) {
-      if(error===null){
+    var movie = req.body;
+    var entryUrl =
+      "https://www.omdbapi.com/?s=" +
+      movie.title +
+      "&type=movie&apikey=" +
+      ombdKey;
+
+    request(entryUrl, function(error, response, body) {
+      if (error === null) {
         res.send(JSON.parse(body));
       } else {
         res.json(error);
@@ -38,28 +45,27 @@ module.exports = function(app, ombd_key) {
 
   // Movie Seen
   app.post("/api/movieseen", function(req, res) {
-    processMovie(req, res, ombd_key, "seen" );
+    processMovie(req, res, ombdKey, "seen");
   });
 
   // Movie Want To Watch
   app.post("/api/movietowatch", function(req, res) {
-    processMovie(req, res, ombd_key, "towatch" );
+    processMovie(req, res, ombdKey, "towatch");
   });
-  
 };
 
-
-function processMovie(req, res, ombd_key, opt ){
-  var movie_imdbID = req.body.imdbID;
+function processMovie(req, res, ombdKey, opt) {
+  var movieImdbID = req.body.imdbID;
   var user = req.body.user;
   // console.log(req.body);
-  let entryUrl = "https://www.omdbapi.com/?i=" + movie_imdbID + "&apikey=" + ombd_key;
-    // console.log(entryUrl);
-  request(entryUrl, function (error, response, body) {
-    if(error===null){
-      var movie = JSON.parse(body)
+  var entryUrl =
+    "https://www.omdbapi.com/?i=" + movieImdbID + "&apikey=" + ombdKey;
+  // console.log(entryUrl);
+  request(entryUrl, function(error, response, body) {
+    if (error === null) {
+      var movie = JSON.parse(body);
       db.Movie.findOrCreate({
-        where: {imdbID: movie.imdbID},
+        where: { imdbID: movie.imdbID },
         defaults: {
           Title: movie.Title,
           Year: movie.Year,
@@ -74,7 +80,7 @@ function processMovie(req, res, ombd_key, opt ){
           Country: movie.Country,
           Awards: movie.Awards,
           Poster: movie.Poster,
-          Genre:  movie.Genre
+          Genre: movie.Genre
         }
       }).spread(function(dbMovie, created) {
         res.json(dbMovie);
@@ -83,8 +89,7 @@ function processMovie(req, res, ombd_key, opt ){
       res.json(error);
     }
   });
-};
-
+}
 
 // imdbID: movie.imdbID,
 // Title: movie.Title,
